401fb3ea6459e15496bcf73816a0486400f165eb
kjayasa <gitdev@jayk.me>
2018-05-04 11:51:10 -0400

    Fixed Settings merge issue ;closes #4

--- src/lib/swaggerProvider/swaggerProvider.ts
+++ src/lib/swaggerProvider/swaggerProvider.ts
@@ -1,16 +1,16 @@
-import { ISettings,settings } from "../settings";
 import * as Swagger from "swagger-schema-official";
-import {FsSwaggerProvider} from "./fsSwaggerProvider";
 import {ILogger} from "../logger";
+import { ISettings, settings } from "../settings";
+import {FsSwaggerProvider} from "./fsSwaggerProvider";
 
 export interface ISwaggerProvider{
-    provide:(settings:ISettings,logger:ILogger)=> Promise<Swagger.Spec>;
+    provide: (settings: ISettings, logger: ILogger) => Promise<Swagger.Spec>;
 }
 
-export function getProvider():ISwaggerProvider{
-    if(settings.swaggerProvider && settings.swaggerProvider.provide){
+export function getProvider(): ISwaggerProvider{
+    if (settings.swaggerProvider && settings.swaggerProvider.provide){
         return settings.swaggerProvider;
-    }else if(settings.swaggerFile){
+    }else if (settings.swaggerFile){
         return new FsSwaggerProvider();
     }else{
         throw new Error("Provide a swagger definition source.");
